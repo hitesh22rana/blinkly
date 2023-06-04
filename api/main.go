48d3f1450e9db248fd20622487009d15e78bd306
hitesh <hitesh22rana@gmail.com>
2023-06-04 18:59:31 +0530

    feat: main.go file setup

---
+++ api/main.go
@@ -0,0 +1,36 @@
+// Path: api\main.go
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	PORT := os.Getenv("APP_PORT")
+
+	app := fiber.New()
+
+	app.use(logger.New())
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(PORT))
+}
